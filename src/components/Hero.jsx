e19fc9d9cfa839d3e664af12f29e43f9ef2cc574
dilshad360 <dilshad360.dev@gmail.com>
2024-07-13 16:29:08 +0530

    i forgot

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,14 +1,16 @@
 import BagIcon from "../assets/svgs/bag.svg";
 import PlayIcon from "../assets/svgs/play.svg";
-// import HeroImage1 from "../assets/images/hero-1.png";
-// import HeroImage2 from "../assets/images/hero-2.png";
-// import HeroImage3 from "../assets/images/hero-3.png";
+import HeroImage1 from "../assets/images/hero-1.png";
+import HeroImage2 from "../assets/images/hero-2.png";
+import HeroImage3 from "../assets/images/hero-3.png";
 import HeroFull from "../assets/images/hero-full.png";
+import { MouseParallaxChild, MouseParallaxContainer } from "react-parallax-mouse";
 
 function Hero() {
     return (
-        <div className="flex flex-col lg:flex-row px-4 md:px-16 lg:px-40 py-8 min-h-[calc(100vh-6rem)]">
-            <div className="flex flex-col items-center lg:items-start w-full lg:w-2/5 text-center lg:text-left">
+        <div className="flex flex-col lg:flex-row px-4 md:px-16 lg:px-40 py-8 min-h-[calc(100vh-6rem)] ">
+            <div className="flex flex-col items-center lg:items-start w-full lg:w-2/5 text-center lg:text-left animated fadeIn">
+            <img className="flex lg:hidden" src={HeroFull} alt="HeroImageFull" />
                 <div className="flex items-center justify-center w-fit shadow-2xl leading-none gap-3 text-secondary px-6 py-3 rounded-xl text-sm">
                     Explore the world! <img src={BagIcon} alt="bag-icon" />{" "}
                 </div>
@@ -29,27 +31,32 @@ function Hero() {
                     </button>
                 </div>
             </div>
-            <div className="relative w-full order-first lg:order-last lg:w-3/5 min-h-[calc(64vh-6rem)] sm:min-h-[calc(80vh-6rem)] px-6 mb-10">
-                <img src={HeroFull} alt="HeroImageFull" />
-                {/* <img
+            <MouseParallaxContainer  globalFactorX={1} globalFactorY={1} className="relative w-3/5  px-6 mb-10 hidden lg:flex  justify-center">
+                <MouseParallaxChild  className="absolute "  factorX={0.03} factorY={0.05}>
+                <img
                     src={HeroImage3}
                     width={640}
                     alt="Image 3"
-                    className="absolute top-0 left-1/2 -translate-x-1/2 object-contain"
+                    className="object-contain "
                 />
+                </MouseParallaxChild>
+                <MouseParallaxChild  className="absolute top-16" factorX={0.07} factorY={0.08}>
                 <img
                     src={HeroImage2}
                     width={420}
                     alt="Image 2"
-                    className="absolute bottom-0 left-1/2 -translate-x-1/2 w-4/5 sm:w-[420px] object-contain"
+                    className="w-4/5 sm:w-[420px] object-contain"
                 />
+                </MouseParallaxChild>
+                <MouseParallaxChild  className="absolute top-36" factorX={0.02} factorY={0.03}>
                 <img
                     src={HeroImage1}
                     width={580}
                     alt="Image 1"
-                    className="absolute bottom-0 left-1/2 -translate-x-1/2  object-contain"
-                /> */}
-            </div>
+                    className=" bottom-0 left-1/2  object-contain"
+                />
+                </MouseParallaxChild>
+                </MouseParallaxContainer>
         </div>
     );
 }
--- src/components/KeyFeatures.jsx
+++ src/components/KeyFeatures.jsx
@@ -14,7 +14,7 @@ function KeyFeatures() {
                         It has roots in a piece of classical Latin literature from 45 BC.
                     </p>
                     <div className='pt-6 px-6 sm:px-0 space-y-5'>
-                        <div className='p-2 flex flex-col sm:flex-row text-left'>
+                        <div className='p-4 flex flex-col sm:flex-row text-left'>
                             <div className='bg-orange-600 p-5 rounded-3xl w-fit'>
                             <img src={LocationIcon} width={32} height={32} alt="location-icon" />
                             </div>
@@ -23,21 +23,21 @@ function KeyFeatures() {
                                 <p className='thin-text text-gray-400 text-sm'>Lorem Ipsum is not simply random text</p>
                             </div>
                         </div>
-                        <div className='p-2 flex flex-col sm:flex-row text-left'>
+                        <div className='p-4 flex flex-col sm:flex-row text-left border w-fit rounded-3xl'>
                             <div className='bg-yellow-500 p-5 rounded-3xl w-fit'>
                             <img src={CalendarIcon} width={32} height={32} alt="location-icon" />
                             </div>
-                            <div className='flex flex-col justify-center pt-4 sm:pt-0 pl-0 sm:pl-6'>
-                                <h4 className='text-xl'>We offer best services</h4>
-                                <p className='thin-text text-gray-400 text-sm'>Lorem Ipsum is not simply random text</p>
+                            <div className='flex flex-col justify-center pt-4 sm:pt-0 pl-0 sm:pl-6 '>
+                                <h4 className='text-xl'>Schedule your trip</h4>
+                                <p className='thin-text text-gray-400 text-sm'>It has roots in a piece of classical</p>
                             </div>
                         </div>
-                        <div className='p-2 flex flex-col sm:flex-row text-left'>
+                        <div className='p-4 flex flex-col sm:flex-row text-left'>
                             <div className='bg-pink-500 p-5 rounded-3xl h-[72px] flex items-center w-fit'>
                             <img src={TicketIcon} width={32} height={32} alt="location-icon" />
                             </div>
                             <div className='flex flex-col justify-center pt-4 sm:pt-0 pl-0 sm:pl-6'>
-                                <h4 className='text-xl'>We offer best services</h4>
+                                <h4 className='text-xl'>Get discounted coupons</h4>
                                 <p className='thin-text text-gray-400 text-sm'>Lorem Ipsum is not simply random text</p>
                             </div>
                         </div>
